38961a9e08edffea33be6dd9b55d4186d95a5c75
Arek Herdzik <arekhbm1@gmail.com>
2021-10-28 11:36:04 +0100

    Creating a list of lessons as seperate JSON array

---
+++ lessons.js
@@ -0,0 +1,72 @@
+lessons = [
+   {
+   "id": 1,
+   "subject": "Maths",     
+   "campus": "London", 
+   "price": 86,
+   "spaces": 5,
+   "image": "https://blogs.glowscotland.org.uk/glowblogs/public/pfeportfolio/uploads/sites/2984/2016/01/Maths-1.png"
+}, {
+   "id": 2,
+   "subject": "Maths", 
+   "campus": "Croydon", 
+   "price": 129,
+   "spaces": 5,
+   "image": "https://blogs.glowscotland.org.uk/glowblogs/public/pfeportfolio/uploads/sites/2984/2016/01/Maths-1.png"
+}, {
+   "id": 3,
+   "subject": "Science", 
+   "campus": "London", 
+   "price": 99,
+   "spaces": 5,
+   "image": "https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/82/34/t-t-10060--science-on-science-bottles-display-cut-outs-_ver_1.avif"
+}, {
+   "id": 4,
+   "subject": "Science", 
+   "campus": "Croydon", 
+   "price": 111,
+   "spaces": 5,
+   "image": "https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/82/34/t-t-10060--science-on-science-bottles-display-cut-outs-_ver_1.avif"
+}, {
+   "id": 5,
+   "subject": "History", 
+   "campus": "London", 
+   "price": 100,
+   "spaces": 5,
+   "image": "https://yt3.ggpht.com/ytc/AKedOLSLKY4Qn-eDK6djQNoret4-sT2ve1zw9jnvVp2_yw=s900-c-k-c0x00ffffff-no-rj"
+}, {
+   "id": 6,
+   "subject": "History", 
+   "campus": "Leeds", 
+   "price": 105,
+   "spaces": 5,
+   "image": "https://yt3.ggpht.com/ytc/AKedOLSLKY4Qn-eDK6djQNoret4-sT2ve1zw9jnvVp2_yw=s900-c-k-c0x00ffffff-no-rj"
+}, {
+   "id": 7,
+   "subject": "Dance", 
+   "campus": "Croydon", 
+   "price": 106,
+   "spaces": 5,
+   "image": "https://ket-uploads-education-ga.s3.amazonaws.com/wp-content/uploads/3616.jpg"
+}, {
+   "id": 8,
+   "subject": "Dance", 
+   "campus": "Wembley", 
+   "price": 114,
+   "spaces": 5,
+   "image": "https://ket-uploads-education-ga.s3.amazonaws.com/wp-content/uploads/3616.jpg"
+}, {
+   "id": 9,
+   "subject": "Drama", 
+   "campus": "Croydon", 
+   "price": 100,
+   "spaces": 5,
+   "image": "https://www.teachwire.net/uploads/news/iStock-856395646_1.jpg"
+}, {
+   "id": 10,
+   "subject": "Drama", 
+   "campus": "Exeter", 
+   "price": 102,
+   "spaces": 5,
+   "image": "https://www.teachwire.net/uploads/news/iStock-856395646_1.jpg"
+}]
