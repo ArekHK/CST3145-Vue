e3d2eca7fb3d9bb2b638eff87ac6759ab4c87b3a
Arek Herdzik <arekhbm1@gmail.com>
2021-11-11 13:22:41 +0000

    Added font Awesome Icon to the website

--- lessons.js
+++ lessons.js
@@ -5,6 +5,7 @@ lessons = [
    "campus": "London", 
    "price": 86,
    "spaces": 5,
+   "icon": "fas fa-calculator",
    "image": "https://blogs.glowscotland.org.uk/glowblogs/public/pfeportfolio/uploads/sites/2984/2016/01/Maths-1.png"
 }, {
    "id": 2,
@@ -12,6 +13,7 @@ lessons = [
    "campus": "Croydon", 
    "price": 129,
    "spaces": 5,
+   "icon": "fas fa-calculator",
    "image": "https://blogs.glowscotland.org.uk/glowblogs/public/pfeportfolio/uploads/sites/2984/2016/01/Maths-1.png"
 }, {
    "id": 3,
@@ -19,6 +21,7 @@ lessons = [
    "campus": "London", 
    "price": 99,
    "spaces": 5,
+   "icon": "fas fa-flask",
    "image": "https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/82/34/t-t-10060--science-on-science-bottles-display-cut-outs-_ver_1.avif"
 }, {
    "id": 4,
@@ -26,6 +29,7 @@ lessons = [
    "campus": "Croydon", 
    "price": 111,
    "spaces": 5,
+   "icon": "fas fa-flask",
    "image": "https://images.twinkl.co.uk/tw1n/image/private/t_630_eco/image_repo/82/34/t-t-10060--science-on-science-bottles-display-cut-outs-_ver_1.avif"
 }, {
    "id": 5,
@@ -33,6 +37,7 @@ lessons = [
    "campus": "London", 
    "price": 100,
    "spaces": 5,
+   "icon": "fas fa-landmark",
    "image": "https://yt3.ggpht.com/ytc/AKedOLSLKY4Qn-eDK6djQNoret4-sT2ve1zw9jnvVp2_yw=s900-c-k-c0x00ffffff-no-rj"
 }, {
    "id": 6,
@@ -40,6 +45,7 @@ lessons = [
    "campus": "Leeds", 
    "price": 105,
    "spaces": 5,
+   "icon": "fas fa-landmark",
    "image": "https://yt3.ggpht.com/ytc/AKedOLSLKY4Qn-eDK6djQNoret4-sT2ve1zw9jnvVp2_yw=s900-c-k-c0x00ffffff-no-rj"
 }, {
    "id": 7,
@@ -47,6 +53,7 @@ lessons = [
    "campus": "Croydon", 
    "price": 106,
    "spaces": 5,
+   "icon": "fas fa-drum",
    "image": "https://ket-uploads-education-ga.s3.amazonaws.com/wp-content/uploads/3616.jpg"
 }, {
    "id": 8,
@@ -54,6 +61,7 @@ lessons = [
    "campus": "Wembley", 
    "price": 114,
    "spaces": 5,
+   "icon": "fas fa-drum",
    "image": "https://ket-uploads-education-ga.s3.amazonaws.com/wp-content/uploads/3616.jpg"
 }, {
    "id": 9,
@@ -61,6 +69,7 @@ lessons = [
    "campus": "Croydon", 
    "price": 100,
    "spaces": 5,
+   "icon": "fas fa-theater-masks",
    "image": "https://www.teachwire.net/uploads/news/iStock-856395646_1.jpg"
 }, {
    "id": 10,
@@ -68,5 +77,6 @@ lessons = [
    "campus": "Exeter", 
    "price": 102,
    "spaces": 5,
+   "icon": "fas fa-theater-masks",
    "image": "https://www.teachwire.net/uploads/news/iStock-856395646_1.jpg"
 }]
